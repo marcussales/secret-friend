9369ee0779b9d86f74cdbb3e23c82533ec18b763
Marcus Sales <marcusvinioliversales@gmail.com>
2024-01-14 13:54:31 -0300

    feat: change space

--- lib/widgets/home/home_form_widget.dart
+++ lib/widgets/home/home_form_widget.dart
@@ -97,7 +97,7 @@ class _HomeFormWidgetState extends State<HomeFormWidget> {
                 ImageConstants.shoppingBags,
                 height: 140,
               ),
-              const SizedBox(height: 45),
+              const SizedBox(height: 90),
             ],
           ),
         ),
